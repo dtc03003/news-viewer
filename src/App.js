35e3ad79ba05f263a2cd3b870be57ad99fb0957b
김동현 <dtc03003@naver.com>
2023-03-01 22:58:38 +0900

    뉴스 뷰어 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage'
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => {
+  return (  
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
+  )
+};
 
 export default App;
